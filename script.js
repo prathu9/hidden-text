eaa6dbcb5ea59e46603d0d979f367a6dd6eed2ba
prathamesh sawant <prathameshpgs@gmail.com>
2022-01-21 23:57:31 +0530

    Hide view box when mouse move out of body.

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const svgCircle = document.querySelector('.view-box circle')
 document.body.addEventListener('mousemove', (e)=> {
    viewBox.setAttribute("style", `position:absolute;top:${e.clientY}px; left:${e.clientX}px`)
 });
+
+document.body.addEventListener('mouseout', (e)=> {
+    viewBox.setAttribute("style", `top:-100%; left:-100%`)
+ });
